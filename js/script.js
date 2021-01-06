f8ccfba660627cf6b67e60b15e6ef592655b1e1f
smaurya <smaurya@craterzone.com>
2021-01-06 12:09:09 +0530

    home page formatted

--- js/script.js
+++ js/script.js
@@ -14,14 +14,7 @@
     });
 
   }
-  $(window).scroll(function() {
-    var height = $(window).scrollTop();
-    if (height > 100) {
-        $('#back2Top').fadeIn();
-    } else {
-        $('#back2Top').fadeOut();
-    }
-});
+
 $(document).ready(function() {
     $("#back2Top").click(function(event) {
         event.preventDefault();
@@ -42,6 +35,16 @@ $(document).ready(function() {
     });
     
 });
+$("#services").click(function(e){
+  e.preventDefault();
+  console.log("services");
+  $('#services_sublist').toggle();
+});
+$("#products").click(function(e){
+  e.preventDefault();
+  $('#products_sublist').toggle();
+});
+
 $(window).scroll(function() {
   var height = $(window).scrollTop();
   if (height > 100) {
